b50f9b94bc23b6b8532624eac15d923ef13a31d4
DiegoVega-uni <189074@iberopuebla.mx>
2021-03-23 16:26:24 -0600

    changes made to admin.py and models.py, database first version of database, timestamp fields autofillable and comment nullable

--- museo/visitas/models.py
+++ museo/visitas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class visitas(models.Model):
+    email = models.EmailField()
+    timestamp_in = models.DateTimeField(auto_now_add = True)
+    timestamp_out = models.DateTimeField(auto_now = True, blank = True)
+    comment = models.CharField(max_length=140, blank = True)
