af356f078a3d20823de2d94c6c2d49f284e0a42d
DiegoVega-uni <189074@iberopuebla.mx>
2021-03-24 17:38:22 -0600

    Adding the null param to date time out

--- museo/visitas/models.py
+++ museo/visitas/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class visitas(models.Model):
     email = models.EmailField()
     timestamp_in = models.DateTimeField(auto_now_add = True)
-    timestamp_out = models.DateTimeField(auto_now = True, blank = True)
+    timestamp_out = models.DateTimeField(auto_now = True, blank = True, null = True)
     comment = models.CharField(max_length=140, blank = True)
